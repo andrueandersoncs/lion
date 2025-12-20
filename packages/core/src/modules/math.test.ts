23f58e5a1d9e3f73ac06493a12a7fcf507977791
Andrue Anderson <andrue.anderson.cs@gmail.com>
2025-12-20 09:12:57 -0500

    feat: further refine math package

---
+++ packages/core/src/modules/math.test.ts
@@ -0,0 +1,41 @@
+import { describe, expect, it } from "bun:test";
+import { math } from "./math";
+import { Effect } from "effect";
+
+describe("math", () => {
+  it("should add two numbers", () => {
+    expect(Effect.runSync(math["+"](1, 2))).toBe(3);
+  });
+
+  it("should subtract two numbers", () => {
+    expect(Effect.runSync(math["-"](1, 2))).toBe(-1);
+  });
+
+  it("should multiply two numbers", () => {
+    expect(Effect.runSync(math["*"](1, 2))).toBe(2);
+  });
+
+  it("should divide two numbers", () => {
+    expect(Effect.runSync(math["/"](1, 2))).toBe(0.5);
+  });
+
+  it("should check if two numbers are equal", () => {
+    expect(Effect.runSync(math["="](1, 1))).toBe(true);
+  });
+
+  it("should check if one number is less than another", () => {
+    expect(Effect.runSync(math["<"](1, 2))).toBe(true);
+  });
+
+  it("should check if one number is greater than another", () => {
+    expect(Effect.runSync(math[">"](1, 2))).toBe(false);
+  });
+
+  it("should check if one number is less than or equal to another", () => {
+    expect(Effect.runSync(math["<="](1, 2))).toBe(true);
+  });
+
+  it("should check if one number is greater than or equal to another", () => {
+    expect(Effect.runSync(math[">="](1, 2))).toBe(false);
+  });
+});
