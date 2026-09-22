3cf2d0bb62cd907d0feb7fa266170a6ebf3836e7
Andrue Anderson <andrue.anderson.cs@gmail.com>
2026-09-22 11:01:23 -0400

    fix(repl): improve graph background visibility

--- packages/repl/src/editor/semantic-graph.tsx
+++ packages/repl/src/editor/semantic-graph.tsx
@@ -1659,7 +1659,7 @@ export function SemanticGraph({
             zoomable
           />
         ) : null}
-        <Background color="var(--crease)" gap={32} size={1} />
+        <Background color="#000" gap={32} size={2} />
       </ReactFlow>
     </div>
   );
